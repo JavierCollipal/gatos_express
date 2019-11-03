96d1db9cbca33e39259f5e350484cbcd080ccc23
Javier Collipal <jcollipal1212@gmail.com>
2019-11-03 19:57:25 -0300

    helmet and ratelimiter added
    Took 33 minutes

--- src/config/express.js
+++ src/config/express.js
@@ -1,7 +1,11 @@
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const helmet = require('helmet');
+const rateLimiter = require('./rateLimiter');
 module.exports = (app, express) => {
   app.use(cors());
+  app.use(helmet());
+  app.use(rateLimiter);
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
