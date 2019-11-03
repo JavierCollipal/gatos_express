9fafae00e4d9fa0a6d6e3fd0b62d9ea7689f4d8c
Javier Collipal <jcollipal1212@gmail.com>
2019-11-03 20:27:40 -0300

    common folder added to structure, response maker functions working
    Took 24 minutes

--- src/controllers/cat.js
+++ src/controllers/cat.js
@@ -1,8 +1,8 @@
 const { catService } = require('../services/index');
-
+const { dataResponse } = require('../common/response');
 const catController = {
   getAll(req, res) {
-    res.send(catService.getAll());
+    res.send(dataResponse(catService.getAll()));
   },
 
   create(req, res) {
