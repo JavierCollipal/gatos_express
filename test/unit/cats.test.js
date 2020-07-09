142489b902049c9fcff7405705ed2ad0ed2f64ac
JavierCollipal <jcollipal1212@gmail.com>
2020-07-08 20:32:03 -0400

    tdd applied in cats routes, now working in the pass test part
    Took 27 minutes

---
+++ test/unit/cats.test.js
@@ -0,0 +1,56 @@
+import app from '../../src/app';
+import request from 'supertest';
+
+const apiPrefix = '/api/v1';
+const defaultUrl = '/cats';
+const defaultCompleteUrl = () => `${apiPrefix}/${defaultUrl}`;
+//we use the cat form post in put and delete call
+let savedCat;
+
+describe('GET /cats', () => {
+	it('should return code 200 and a array with/without data', (done) => {
+		request(app)
+			.get(defaultCompleteUrl())
+			.then((response) => {
+				const { cats } = response.body;
+				expect(response.statusCode).toBe(200);
+				expect(cats.length).toBeGreaterThanOrEqual(0);
+				done();
+			});
+	});
+});
+describe('POST /cats', () => {
+	it('should return code 201 and the saved cat', (done) => {
+		request(app)
+			.post(defaultCompleteUrl())
+			.then((response) => {
+				const { cat } = response.body;
+				savedCat = cat;
+				expect(response.statusCode).toBe(201);
+				expect(cat).toBeDefined();
+				done();
+			});
+	});
+});
+describe('PUT /cats/:id', () => {
+	it('should return code 201 and the updated cat', (done) => {
+		request(app)
+			.put(`${defaultCompleteUrl()}/${savedCat._id}`)
+			.then((response) => {
+				const { cat } = response.body;
+				expect(response.statusCode).toBe(201);
+				expect(cat).toBeDefined();
+				done();
+			});
+	});
+});
+describe('DELETE /cats', () => {
+	it('should return code 200 after deleting the cat', (done) => {
+		request(app)
+			.delete(`${defaultCompleteUrl()}/${savedCat._id}`)
+			.then((response) => {
+				expect(response.statusCode).toBe(200);
+				done();
+			});
+	});
+});
